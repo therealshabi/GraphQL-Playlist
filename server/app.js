65aeaece38ba3fd760dcb194e38063c4896fd5fc
Shahbaz Hussain <shahbaz.h96@gmail.com>
2018-06-20 15:49:59 +0530

    Added Schema and the Root Query

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const graphqlHTTP =  require('express-graphql');
+const schema = require('./schema/schema.js')
 
 const app = express();
+
+app.use("/graphql", graphqlHTTP({
+  schema
+}));
 
 app.listen(4000, () => {
   console.log("Server running on port 4000...");
