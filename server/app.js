788773c9a83366c7133744136d7a1d1fed2a6b26
Shahbaz Hussain <shahbaz.h96@gmail.com>
2018-06-21 12:07:43 +0530

    Added Database Models using mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP =  require('express-graphql');
 const schema = require('./schema/schema')
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect("mongodb://therealshabi:test123@ds261660.mlab.com:61660/graphql_ninja");
+mongoose.connection.once('open', ()=> {
+  console.log("connected to database");
+});
 
 app.use("/graphql", graphqlHTTP({
   schema,
