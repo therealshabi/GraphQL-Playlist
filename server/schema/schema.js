5048d6bdb036f147ae458543f8abf3e826c680fa
Shahbaz Hussain <shahbaz.h96@gmail.com>
2018-06-21 10:23:54 +0530

    Added AuthorType and connected that with Books

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,15 +5,26 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLSchema,
-  GraphQLID
+  GraphQLID,
+  GraphQLInt
  } = graphql;
 
 // dummy data
 var books = [
-  {name:"Harry Potter", genre: "Fiction", id:"1"},
-  {name:"The Final Empire", genre: "Fantasy", id:"2"},
-  {name:"The Long Earth", genre: "Sci-Fi", id:"3"}
+  {name:"Harry Potter", genre: "Fiction", id:"1", authorId:'1'},
+  {name:"The Final Empire", genre: "Fantasy", id:"2",authorId:'2'},
+  {name:"The Long Earth", genre: "Sci-Fi", id:"3",authorId:'3'},
+  {name:"The Hero of Ages", genre: "Fantasy", id:"4",authorId:'2'},
+  {name:"The Colour of Magic", genre: "Fantasy", id:"5",authorId:'3'},
+  {name:"The Light Fantastic", genre: "Fantasy", id:"6",authorId:'3'}
 ];
+
+var authors = [
+  {name:"J.K. Rowling", age:52, id:"1"},
+  {name:"Brandon Sanderson", age:42, id:"2"},
+  {name:"Terry Pratchett", age:66, id:"3"}
+];
+
 
 const BookType = new GraphQLObjectType({
   name :'Book',
@@ -21,8 +32,24 @@ const BookType = new GraphQLObjectType({
     id: {type: GraphQLID},
     name: {type: GraphQLString},
     genre: {type: GraphQLString},
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, {id: parent.authorId});
+      }
+    }
   })
 });
+
+const AuthorType = new GraphQLObjectType({
+  name:'Author',
+  fields: () => ({
+    id: {type: GraphQLID},
+    name: {type: GraphQLString},
+    age: {type: GraphQLInt}
+  })
+});
+
 
 const rootQuery = new GraphQLObjectType({
   name :'RootQueryType',
@@ -36,9 +63,20 @@ const rootQuery = new GraphQLObjectType({
         // code to get data from db/ othersource
         return _.find(books, {id: args.id});
       }
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: {type : GraphQLID}
+      },
+      resolve(parent, args) {
+        // code to get data from db/ othersource
+        return _.find(authors, {id: args.id});
+      }
     }
   }
 });
+
 
 module.exports = new GraphQLSchema({
   query: rootQuery
